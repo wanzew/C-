8fe51fb1f8f2649a3795eae966e30894f72e2c1f
wanzew <wanzew@163.com>
2020-06-06 14:56:35 +0800

    init commit

---
+++ Heap/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"maxHeap.hpp"
+#include"heapSortInplace.hpp"
+#include"priorityQueue.hpp"
+using namespace std;
+
+int main() {
+
+	priorityQueue::subMain();
+	maxHeap::maxHeapMain();
+
+	system("pause");
+	return 0;
+}
